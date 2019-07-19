977c15d0f2015b8b4f829e1089f6dd17a5b3dc83
Sergei Makarov <sergeimakarovweb@gmail.com>
2019-07-19 20:31:57 +0000

    add get_appmetrica_data

--- R/appmetricaR.R
+++ R/appmetricaR.R
@@ -1,5 +1,4 @@
 #' @title Get non-aggregated data from your AppMetrica application
-
 #' @description This function allows you to obtain raw data from your AppMetrica application. The AppMetrica Logs API accepts the request and puts it in the queue. If the request is processed successfully, AppMetrica prepares a file for download. In this case, the API returns the HTTP 202 Accepted status. If the request resulted in an error, the API returns the appropriate response status, and the HTTP message body contains the error description.
 #' @param type API resources
 #' @param application_id 	Unique numeric identifier for the application in AppMetrica
@@ -68,6 +67,118 @@ get_appmetrica_logs <- function(
 
 }
 
+#' @title Get data from your AppMetrica application
+#' @description The Reporting API allows you to get application traffic statistics and other data without using the AppMetrica interface. You can create the desired report structure by specifying metrics and dimensions in the API request.
+#' @param ids Comma-separated list of counter numbers.
+#' @param date1 Start date of the report period in the format YYYY-MM-DD. You can also use the values: today, yesterday, ndaysAgo. Default value: 6daysAgo
+#' @param date2 End date of the report period in the format YYYY-MM-DD. You can also use the values: today, yesterday, ndaysAgo. Default value: today
+#' @param metrics Comma-separated list of metrics. Limit: 20 metrics per request.
+#' @param dimensions 	Comma-separated list of dimensions. Limit: 10 dimensions per request.
+#' @param filters Segmentation filter. Limit: Maximum of 10 unique dimensions and metrics, 20 separate filters, and 10,000 characters per filter string.
+#' @param group Grouping data by time. Default value: week
+#' @param accuracy Accuracy of results. Allows you to manage sampling (the number of visits used to calculate the final value). Default value: full
+#' @param include_undefined Outputs rows that don't have defined dimension values. This only affects the first dimension. Disabled by default.
+#' @param lang Language.
+#' @param token An access token from the Yandex.OAuth service
+#' @return The requested data will be returned as a data frame.
+#' @export
+#' @examples
+#' \dontrun{
+#' get_appmetrica_data (
+#' ids = "130830,130825",
+#' date1 = "2018-01-01",
+#' date2 = "2018-02-02",
+#' metrics = "ym:ge:users",
+#' dimensions = "ym:ge:mobileDeviceBranding,ym:ge:mobileDeviceModel,ym:ge:appID,ym:ge:date"
+#' )
+#'}
+
+get_appmetrica_data <- function(
+  ids = NULL,
+  date1 = NULL,
+  date2 = NULL,
+  metrics = "ym:ge:users",
+  dimensions = "ym:ge:appID",
+  filters = NULL,
+  group = NULL,
+  accuracy = "full",
+  include_undefined = NULL,
+  lang = NULL,
+  token = NULL
+){
+
+  if (is.null(token)) {
+    token <- appmetrica_auth()
+  }
+
+  #check if query has an error
+  request <- GET(url = "https://api.appmetrica.yandex.ru/stat/v1/data",
+                 query = list(
+                   ids = ids,
+                   date1 = date1,
+                   date2 = date2,
+                   metrics = gsub("[\\s\\n\\t]", "", metrics, perl = TRUE),
+                   dimensions = gsub("[\\s\\n\\t]", "", dimensions, perl = TRUE),
+                   filters = filters,
+                   group = group,
+                   accuracy = "full",
+                   include_undefined = include_undefined,
+                   lang = tolower(lang),
+                   limit = 1
+                 ),
+                 add_headers(Authorization = paste0("OAuth ", token)))
+
+  answer <- content(request, type = "application/json")
+
+  if (http_error(request)) {
+    stop(answer$message)
+  }
+
+  limit    <- 99998
+  offset   <- 1
+
+  rows_in_sample <- 1
+
+  list_of_samples <- list()
+  counter <- 1
+
+  #collect samples until we run out of rows in a sample
+  while (rows_in_sample > 0) {
+    request <- GET(url = "https://api.appmetrica.yandex.ru/stat/v1/data.csv",
+                   query = list(
+                     ids = ids,
+                     date1 = date1,
+                     date2 = date2,
+                     metrics = gsub("[\\s\\n\\t]", "", metrics, perl = TRUE),
+                     dimensions = gsub("[\\s\\n\\t]", "", dimensions, perl = TRUE),
+                     filters = filters,
+                     group = group,
+                     accuracy = "full",
+                     include_undefined = include_undefined,
+                     lang = tolower(lang),
+                     limit = limit,
+                     offset = offset
+                   ),
+                   add_headers(Authorization = paste0("OAuth ", token)))
+
+    answer <- content(request, as = "parsed", "text/csv")[-1, ]
+
+    rows_in_sample <- nrow(answer)
+    offset <- limit + offset
+
+    # add data to list_of_samples if we have avaliable rows
+    if (rows_in_sample > 0) {
+      list_of_samples[[counter]] <- answer
+      counter <- counter + 1
+    }
+
+  }
+
+  final_df <- do.call("rbind", list_of_samples)
+
+  return(final_df)
+}
+
 
 appmetrica_auth <- function(token.path = getwd()) {
 
@@ -98,4 +209,3 @@ appmetrica_auth <- function(token.path = getwd()) {
   return(token)
 
 }
-
